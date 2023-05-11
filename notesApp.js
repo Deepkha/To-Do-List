63dea1d2311dc0010345c3d622e27343e2fb65d8
Deepkha <deepeshkhatri902@gmail.com>
2023-05-11 20:42:33 +0530

    first commit

---
+++ notesApp.js
@@ -0,0 +1,7 @@
+addTitle=document.getElementById('addTitle');
+const addText=document.getElementById('addText');
+const addNoteButton=document.getElementById('addNote');
+const noteDiv=document.getElementById('notes');
+addNoteButton.addEventListener('click', () => {
+    console.log('you are adding your notes')
+})
